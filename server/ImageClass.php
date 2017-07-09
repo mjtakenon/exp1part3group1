9feeb53965cd33ef5f664deecfef6c115350a9e0
mjtakenon <takenon777@gmail.com>
2017-07-10 02:31:53 +0900

    fix

--- server/ImageClass.php
+++ server/ImageClass.php
@@ -201,7 +201,6 @@ class ImageAnalizer
             {
                 for($divx = 0; $divx < $divwidth; $divx++)
                 {
-                    var_dump($this->getSumRGB($image,$divx*$divedwidth,$divy*$divedheight,$divedwidth,$divedheight));
                     $tmpRGB[$divx][$divy]->setRGB($this->getSumRGB($image,$divx*$divedwidth,$divy*$divedheight,$divedwidth,$divedheight));
                 }
             }
