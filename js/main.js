da9895322b434dded429f82bb663447e1c48604e
uoo38 <plus.wisteria38@gmail.com>
2017-07-10 02:54:02 +0900

    add table that show mosaic art

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$('#file_input').change(function () {
-  $('#dummy_file').val($(this).val().replace("C:\\fakepath\\", ""));
+const width  = 4;
+const height = 4;
+
+function init() {
+  let s = '';
+  for (let i = 0; i < height; i++) {
+    s += '<tr>'
+    for (let j = 0; j < width; j++) {
+      s += '<td></td>'
+    }
+    s += '</tr>'
+  }
+  $('#mosaic > tbody').html(s);
+}
+
+init();
+
+$("#file_input").change(function () {
+  $("#dummy_file").val($(this).val().replace("C:\\fakepath\\", ""));
 });
