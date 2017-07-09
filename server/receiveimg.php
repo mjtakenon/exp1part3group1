0384d932b6493c07cfa17c44b8a3622a5b067bf9
mjtakenon <takenon777@gmail.com>
2017-07-09 16:30:28 +0900

    showdata

--- server/receiveimg.php
+++ server/receiveimg.php
@@ -8,40 +8,30 @@
 		<h1>結果だっていってるだろぉぉぉぉぉん！？</h1>
 		<?php
 			if(is_uploaded_file($_FILES["upfile"]["tmp_name"])){
-				echo "filetype = ".$_FILES["upfile"]["type"];
-				echo $_FILES["upfile"]["type"];
-
-
-				list($img_width,$img_height,$mime_type,$attr) = getimagesize($_FILES["upfile"]["tmp_name"]);
+				list($width,$height,$mime_type,$attr) = getimagesize($_FILES["upfile"]["tmp_name"]);
 
 				switch($mime_type){
 					//jpegの場合
 					case IMAGETYPE_JPEG:
 						//拡張子の設定
-						$img_extension = "jpg";
+						$ext = "jpg";
 						break;
 					//pngの場合
 					case IMAGETYPE_PNG:
 					//拡張子の設定
-						$img_extension = "png";
+						$ext = "png";
 						break;
 					//gifの場合
 					case IMAGETYPE_GIF:
 						//拡張子の設定
-						$img_extension = "gif";
+						$ext = "gif";
 						break;
 				}
 				//拡張子の出力
-				echo $img_extension;
 
-				//if($image["type"])
-				/*if(move_uploaded_file($_FILES["upfile"]["tmp_name"],"files/".$_FILES["upfile"]["name"])){
-					chmod("files/".$_FILES["upfile"]["name"],0777);
-					echo $_FILES["upfile"]["name"]."をアップロードしました。";
-				}
-				else{
-					echo "ファイルをアップロードできません";
-				}*/
+				echo "x=".$width."y=".$height."attr=".$attr."<br>";
+				echo "ext=".$ext."<br>";
+
 			}
 			else {
 				echo "ファイルが選択されていません。";
