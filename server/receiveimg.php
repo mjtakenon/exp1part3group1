df6853d82150d8ef25013e33079346bc0071f2d7
mjtakenon <takenon777@gmail.com>
2017-07-09 16:24:53 +0900

    update

--- server/receiveimg.php
+++ server/receiveimg.php
@@ -7,10 +7,11 @@
 	<body>
 		<h1>結果だっていってるだろぉぉぉぉぉん！？</h1>
 		<?php
-			//if(is_uploaded_file($_FILES["upfile"]["tmp_name"])){
-				//echo "filetype = ".$_FILES["upfile"]["type"];
-				//echo $_FILES["upfile"]["type"];
-/*
+			if(is_uploaded_file($_FILES["upfile"]["tmp_name"])){
+				echo "filetype = ".$_FILES["upfile"]["type"];
+				echo $_FILES["upfile"]["type"];
+
+
 				list($img_width,$img_height,$mime_type,$attr) = getimagesize($_FILES["upfile"]["tmp_name"]);
 
 				switch($mime_type){
@@ -32,7 +33,7 @@
 				}
 				//拡張子の出力
 				echo $img_extension;
-*/
+
 				//if($image["type"])
 				/*if(move_uploaded_file($_FILES["upfile"]["tmp_name"],"files/".$_FILES["upfile"]["name"])){
 					chmod("files/".$_FILES["upfile"]["name"],0777);
@@ -41,11 +42,10 @@
 				else{
 					echo "ファイルをアップロードできません";
 				}*/
-			/*}
+			}
 			else {
 				echo "ファイルが選択されていません。";
-			}*/
-			echo "ok";
+			}
 		?>
 	</body>
 
