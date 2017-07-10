451e8348170f9f50f0411de5db67a15c59d8e7aa
uoo38 <plus.wisteria38@gmail.com>
2017-07-10 12:15:02 +0900

    add animation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-const width  = 4;
+const width = 4;
 const height = 4;
 
 function init() {
   let s = '';
   for (let i = 0; i < height; i++) {
-    s += '<tr>'
+    s += '<tr class="y' + i + '">'
     for (let j = 0; j < width; j++) {
-      s += '<td></td>'
+      s += '<td class="x' + j + '"></td>';
     }
     s += '</tr>'
   }
   $('#mosaic > tbody').html(s);
+
+  for (let i = 0; i < height; i++)
+    for (let j = 0; j < width; j++)
+      $('.y' + i + ' > .x' + j).addClass('empty-cell', 3000, 'linear');
+
 }
 
 init();
