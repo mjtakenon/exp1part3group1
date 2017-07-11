814282abfb1304805fcdefb928dae316f44ceb45
uoo38 <plus.wisteria38@gmail.com>
2017-07-11 09:29:00 +0900

    add setImage function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const width = 4;
+const width  = 4;
 const height = 4;
 
 function init() {
@@ -15,10 +15,31 @@ function init() {
   for (let i = 0; i < height; i++)
     for (let j = 0; j < width; j++)
       $('.y' + i + ' > .x' + j).addClass('empty-cell', 3000, 'linear');
+}
 
+function setImage(x, y, url) {
+  const e = $('.y' + y + ' > .x' + x);
+  e.removeClass('empty-cell', 1500, 'linear', function () { // 消えるアニメーション
+    // 追加する部分のCSS
+    e.css({
+      background: 'url(' + url + ')',
+      'background-size': 'cover',
+      opacity: 0
+    });
+    // 追加部分のアニメーション
+    e.animate({
+      opacity: '1'
+    }, 1500);
+  });
+}
+
+// for debugging.
+function debug() {
+  setImage(2, 2, 'https://pbs.twimg.com/media/B0wx8kpCAAAfFjx.jpg');
 }
 
 init();
+debug();
 
 $("#file_input").change(function () {
   $("#dummy_file").val($(this).val().replace("C:\\fakepath\\", ""));
